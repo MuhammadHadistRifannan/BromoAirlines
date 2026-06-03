6a1a958e57d83ec5ca1e36df5c9f3b481d544127
MuhammadHadistRifannan <ipanjabar933@gmail.com>
2026-06-03 13:32:44 +0700

    update sidebar

--- Forms/Sidebar/DashboardForm.Designer.cs
+++ Forms/Sidebar/DashboardForm.Designer.cs
@@ -28,30 +28,34 @@ partial class DashboardForm
     {
         System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DashboardForm));
         sidebarPanel = new Panel();
-        contentPanel = new Panel();
-        pictureBox1 = new PictureBox();
-        pictureBox2 = new PictureBox();
-        label1 = new Label();
-        label2 = new Label();
-        button1 = new Button();
-        label3 = new Label();
-        button2 = new Button();
-        label4 = new Label();
-        button3 = new Button();
-        label5 = new Label();
-        button4 = new Button();
+        label7 = new Label();
+        button6 = new Button();
         label6 = new Label();
         button5 = new Button();
-        button6 = new Button();
-        label7 = new Label();
+        label5 = new Label();
+        button4 = new Button();
+        label4 = new Label();
+        button3 = new Button();
+        label3 = new Label();
+        button2 = new Button();
+        label2 = new Label();
+        label1 = new Label();
+        pictureBox2 = new PictureBox();
+        pictureBox1 = new PictureBox();
+        button1 = new Button();
+        contentPanel = new Panel();
+        button7 = new Button();
+        label8 = new Label();
         sidebarPanel.SuspendLayout();
-        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
         ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
         SuspendLayout();
         // 
         // sidebarPanel
         // 
         sidebarPanel.BackColor = SystemColors.Control;
+        sidebarPanel.Controls.Add(label8);
+        sidebarPanel.Controls.Add(button7);
         sidebarPanel.Controls.Add(label7);
         sidebarPanel.Controls.Add(button6);
         sidebarPanel.Controls.Add(label6);
@@ -71,69 +75,88 @@ partial class DashboardForm
         sidebarPanel.Location = new Point(0, 0);
         sidebarPanel.Margin = new Padding(4, 5, 4, 5);
         sidebarPanel.Name = "sidebarPanel";
-        sidebarPanel.Size = new Size(371, 1267);
+        sidebarPanel.Size = new Size(371, 1421);
         sidebarPanel.TabIndex = 0;
         // 
-        // contentPanel
+        // label7
         // 
-        contentPanel.Dock = DockStyle.Fill;
-        contentPanel.Location = new Point(371, 0);
-        contentPanel.Margin = new Padding(4, 5, 4, 5);
-        contentPanel.Name = "contentPanel";
-        contentPanel.Size = new Size(1343, 1267);
-        contentPanel.TabIndex = 2;
+        label7.AutoSize = true;
+        label7.BackColor = Color.Transparent;
+        label7.Font = new Font("Segoe UI", 12F);
+        label7.Location = new Point(131, 1323);
+        label7.Name = "label7";
+        label7.Size = new Size(89, 32);
+        label7.TabIndex = 14;
+        label7.Text = "Logout";
         // 
-        // pictureBox1
+        // button6
         // 
-        pictureBox1.BackColor = Color.Transparent;
-        pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-        pictureBox1.Location = new Point(32, 26);
-        pictureBox1.Name = "pictureBox1";
-        pictureBox1.Size = new Size(306, 295);
-        pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
-        pictureBox1.TabIndex = 0;
-        pictureBox1.TabStop = false;
+        button6.BackColor = Color.Transparent;
+        button6.Location = new Point(32, 1298);
+        button6.Name = "button6";
+        button6.Size = new Size(317, 87);
+        button6.TabIndex = 13;
+        button6.UseVisualStyleBackColor = false;
         // 
-        // pictureBox2
+        // label6
         // 
-        pictureBox2.BackColor = Color.Transparent;
-        pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
-        pictureBox2.Location = new Point(32, 286);
-        pictureBox2.Name = "pictureBox2";
-        pictureBox2.Size = new Size(306, 127);
-        pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
-        pictureBox2.TabIndex = 1;
-        pictureBox2.TabStop = false;
+        label6.AutoSize = true;
+        label6.BackColor = Color.Transparent;
+        label6.Font = new Font("Segoe UI", 12F);
+        label6.Location = new Point(131, 1112);
+        label6.Name = "label6";
+        label6.Size = new Size(69, 32);
+        label6.TabIndex = 11;
+        label6.Text = "Profil";
         // 
-        // label1
+        // button5
         // 
-        label1.AutoSize = true;
-        label1.Font = new Font("Segoe UI", 12F);
-        label1.Location = new Point(32, 447);
-        label1.Name = "label1";
-        label1.Size = new Size(82, 32);
-        label1.TabIndex = 2;
-        label1.Text = "MENU";
+        button5.BackColor = Color.Transparent;
+        button5.Location = new Point(32, 1087);
+        button5.Name = "button5";
+        button5.Size = new Size(317, 87);
+        button5.TabIndex = 12;
+        button5.UseVisualStyleBackColor = false;
         // 
-        // label2
+        // label5
         // 
-        label2.AutoSize = true;
-        label2.BackColor = Color.Transparent;
-        label2.Font = new Font("Segoe UI", 12F);
-        label2.Location = new Point(131, 531);
-        label2.Name = "label2";
-        label2.Size = new Size(129, 32);
-        label2.TabIndex = 3;
-        label2.Text = "Dashboard";
+        label5.AutoSize = true;
+        label5.BackColor = Color.Transparent;
+        label5.Font = new Font("Segoe UI", 12F);
+        label5.Location = new Point(131, 1000);
+        label5.Name = "label5";
+        label5.Size = new Size(95, 32);
+        label5.TabIndex = 9;
+        label5.Text = "Riwayat";
         // 
-        // button1
+        // button4
         // 
-        button1.BackColor = Color.Transparent;
-        button1.Location = new Point(32, 506);
-        button1.Name = "button1";
-        button1.Size = new Size(317, 87);
-        button1.TabIndex = 4;
-        button1.UseVisualStyleBackColor = false;
+        button4.BackColor = Color.Transparent;
+        button4.Location = new Point(32, 975);
+        button4.Name = "button4";
+        button4.Size = new Size(317, 87);
+        button4.TabIndex = 10;
+        button4.UseVisualStyleBackColor = false;
+        // 
+        // label4
+        // 
+        label4.AutoSize = true;
+        label4.BackColor = Color.Transparent;
+        label4.Font = new Font("Segoe UI", 12F);
+        label4.Location = new Point(131, 889);
+        label4.Name = "label4";
+        label4.Size = new Size(135, 32);
+        label4.TabIndex = 7;
+        label4.Text = "Pemesanan";
+        // 
+        // button3
+        // 
+        button3.BackColor = Color.Transparent;
+        button3.Location = new Point(32, 864);
+        button3.Name = "button3";
+        button3.Size = new Size(317, 87);
+        button3.TabIndex = 8;
+        button3.UseVisualStyleBackColor = false;
         // 
         // label3
         // 
@@ -155,91 +178,92 @@ partial class DashboardForm
         button2.TabIndex = 6;
         button2.UseVisualStyleBackColor = false;
         // 
-        // label4
+        // label2
         // 
-        label4.AutoSize = true;
-        label4.BackColor = Color.Transparent;
-        label4.Font = new Font("Segoe UI", 12F);
-        label4.Location = new Point(131, 747);
-        label4.Name = "label4";
-        label4.Size = new Size(135, 32);
-        label4.TabIndex = 7;
-        label4.Text = "Pemesanan";
+        label2.AutoSize = true;
+        label2.BackColor = Color.Transparent;
+        label2.Font = new Font("Segoe UI", 12F);
+        label2.Location = new Point(131, 531);
+        label2.Name = "label2";
+        label2.Size = new Size(129, 32);
+        label2.TabIndex = 3;
+        label2.Text = "Dashboard";
         // 
-        // button3
+        // label1
         // 
-        button3.BackColor = Color.Transparent;
-        button3.Location = new Point(32, 722);
-        button3.Name = "button3";
-        button3.Size = new Size(317, 87);
-        button3.TabIndex = 8;
-        button3.UseVisualStyleBackColor = false;
+        label1.AutoSize = true;
+        label1.Font = new Font("Segoe UI", 12F);
+        label1.Location = new Point(32, 447);
+        label1.Name = "label1";
+        label1.Size = new Size(82, 32);
+        label1.TabIndex = 2;
+        label1.Text = "MENU";
         // 
-        // label5
+        // pictureBox2
         // 
-        label5.AutoSize = true;
-        label5.BackColor = Color.Transparent;
-        label5.Font = new Font("Segoe UI", 12F);
-        label5.Location = new Point(131, 858);
-        label5.Name = "label5";
-        label5.Size = new Size(95, 32);
-        label5.TabIndex = 9;
-        label5.Text = "Riwayat";
+        pictureBox2.BackColor = Color.Transparent;
+        pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+        pictureBox2.Location = new Point(32, 286);
+        pictureBox2.Name = "pictureBox2";
+        pictureBox2.Size = new Size(306, 127);
+        pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+        pictureBox2.TabIndex = 1;
+        pictureBox2.TabStop = false;
         // 
-        // button4
+        // pictureBox1
         // 
-        button4.BackColor = Color.Transparent;
-        button4.Location = new Point(32, 833);
-        button4.Name = "button4";
-        button4.Size = new Size(317, 87);
-        button4.TabIndex = 10;
-        button4.UseVisualStyleBackColor = false;
+        pictureBox1.BackColor = Color.Transparent;
+        pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+        pictureBox1.Location = new Point(32, 26);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(306, 295);
+        pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
         // 
-        // label6
+        // button1
         // 
-        label6.AutoSize = true;
-        label6.BackColor = Color.Transparent;
-        label6.Font = new Font("Segoe UI", 12F);
-        label6.Location = new Point(131, 970);
-        label6.Name = "label6";
-        label6.Size = new Size(69, 32);
-        label6.TabIndex = 11;
-        label6.Text = "Profil";
+        button1.BackColor = Color.Transparent;
+        button1.Location = new Point(32, 506);
+        button1.Name = "button1";
+        button1.Size = new Size(317, 87);
+        button1.TabIndex = 4;
+        button1.UseVisualStyleBackColor = false;
         // 
-        // button5
+        // contentPanel
         // 
-        button5.BackColor = Color.Transparent;
-        button5.Location = new Point(32, 945);
-        button5.Name = "button5";
-        button5.Size = new Size(317, 87);
-        button5.TabIndex = 12;
-        button5.UseVisualStyleBackColor = false;
+        contentPanel.Dock = DockStyle.Fill;
+        contentPanel.Location = new Point(371, 0);
+        contentPanel.Margin = new Padding(4, 5, 4, 5);
+        contentPanel.Name = "contentPanel";
+        contentPanel.Size = new Size(1343, 1421);
+        contentPanel.TabIndex = 2;
         // 
-        // button6
+        // button7
         // 
-        button6.BackColor = Color.Transparent;
-        button6.Location = new Point(32, 1106);
-        button6.Name = "button6";
-        button6.Size = new Size(317, 87);
-        button6.TabIndex = 13;
-        button6.UseVisualStyleBackColor = false;
+        button7.BackColor = Color.Transparent;
+        button7.Location = new Point(32, 740);
+        button7.Name = "button7";
+        button7.Size = new Size(317, 87);
+        button7.TabIndex = 15;
+        button7.UseVisualStyleBackColor = false;
         // 
-        // label7
+        // label8
         // 
-        label7.AutoSize = true;
-        label7.BackColor = Color.Transparent;
-        label7.Font = new Font("Segoe UI", 12F);
-        label7.Location = new Point(131, 1131);
-        label7.Name = "label7";
-        label7.Size = new Size(89, 32);
-        label7.TabIndex = 14;
-        label7.Text = "Logout";
+        label8.AutoSize = true;
+        label8.BackColor = Color.Transparent;
+        label8.Font = new Font("Segoe UI", 12F);
+        label8.Location = new Point(131, 765);
+        label8.Name = "label8";
+        label8.Size = new Size(100, 32);
+        label8.TabIndex = 16;
+        label8.Text = "Bandara";
         // 
         // DashboardForm
         // 
         AutoScaleDimensions = new SizeF(10F, 25F);
         AutoScaleMode = AutoScaleMode.Font;
-        ClientSize = new Size(1714, 1267);
+        ClientSize = new Size(1714, 1421);
         Controls.Add(contentPanel);
         Controls.Add(sidebarPanel);
         Margin = new Padding(4, 5, 4, 5);
@@ -249,8 +273,8 @@ partial class DashboardForm
         WindowState = FormWindowState.Maximized;
         sidebarPanel.ResumeLayout(false);
         sidebarPanel.PerformLayout();
-        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
         ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+        ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
         ResumeLayout(false);
     }
 
@@ -286,4 +310,6 @@ partial class DashboardForm
     private Button button5;
     private Label label5;
     private Button button4;
+    private Label label8;
+    private Button button7;
 }
